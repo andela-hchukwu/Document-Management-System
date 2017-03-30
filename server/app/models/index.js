0947c6b241ce10c7607e62b5ebab61376400d19c
henry <henry.chukwu@andela.com>
2017-03-30 18:08:35 +0100

    chore(models-index.js): use tenary operator [Fix: #142307073]

--- server/app/models/index.js
+++ server/app/models/index.js
@@ -1,6 +1,6 @@
-import fs from 'fs';
-import path from 'path';
-import Sequelize from 'sequelize';
+const fs = require('fs');
+const path = require('path');
+const Sequelize = require('sequelize');
 
 const basename = path.basename(module.filename);
 const env = process.env.NODE_ENV || 'development';
@@ -8,14 +8,9 @@ const config = require('../../config/config.json')[env];
 
 const db = {};
 
-let sequelize;
-if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable]);
-} else {
-  sequelize = new Sequelize(
-    config.database, config.username, config.password, config
-  );
-}
+const sequelize = (config.use_env_variable) ?
+  new Sequelize(process.env[config.use_env_variable]) :
+  new Sequelize(config.database, config.username, config.password, config);
 
 
 fs
@@ -30,9 +25,8 @@ fs
   });
 
 Object.keys(db).forEach((modelName) => {
-  if (db[modelName].associate) {
+  if (db[modelName].associate)
     db[modelName].associate(db);
-  }
 });
 
 db.sequelize = sequelize;

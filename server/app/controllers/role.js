63b39176cf86d1e6c2ddfe3975cba58181d32391
henry <henry.chukwu@andela.com>
2017-03-31 14:49:39 +0100

    chore(role-controller): add role controller method to create new roles [Fix: #142272055]

--- server/app/controllers/role.js
+++ server/app/controllers/role.js
@@ -18,9 +18,25 @@ class RolesController {
    */
   static getRoles(req, res) {
     model.Role.findAll()
-            .then(roles => res.status(200)
-                    .send(roles)
-            );
+      .then(roles => res.status(200)
+        .send(roles));
+  }
+
+
+  /**
+   * Method createRole
+   * @param {Object} req - request object
+   * @param {Object} res - response object
+   * @return {Object} roles object
+   *
+   * @memberOf RolesController
+   */
+  static createRole(req, res) {
+    model.Role.create(req.body)
+      .then(newRole => res.status(201)
+        .send(newRole))
+      .catch(error => res.status(400)
+        .send(error.errors));
   }
 }
 

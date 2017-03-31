c8c5298c43d2ec1998862fdd7b4f06026d4da0e2
henry <henry.chukwu@andela.com>
2017-03-31 15:51:27 +0100

    chore(role-controller): add role controller method to obtain role for specific user [Fix: #142272055]

--- server/app/controllers/role.js
+++ server/app/controllers/role.js
@@ -38,6 +38,25 @@ class RolesController {
       .catch(error => res.status(400)
         .send(error.errors));
   }
+
+  /**
+   * Method to get specific role
+   * @param {object} req - request object
+   * @param {object} res - response object
+   * @return {object} response message
+   *
+   * @memberOf RolesController
+   */
+  static getRole(req, res) {
+    model.Role.findById(req.params.id)
+      .then((role) => {
+        if (!role) return res.status(404)
+            .send({ message: `No role with id ${req.params.role}` });
+
+        return res.status(200)
+          .send(role);
+      });
+  }
 }
 
-module.export = RolesController;
+module.exports = RolesController;

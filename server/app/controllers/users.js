e7de20c6089c2ec6f19aa06a40cb657c73fb93ab
henry <henry.chukwu@andela.com>
2017-04-01 16:30:04 +0100

    feature(users-controller): add users controller to create a new user [Delivers: #142272055]

--- server/app/controllers/users.js
+++ server/app/controllers/users.js
@@ -28,6 +28,34 @@ class UsersController {
     }).then(users => res.status(200)
         .send(users));
   }
+
+  /**
+   *
+   * createUsers method
+   * @param {object} req - request object
+   * @param {object} res - response object
+   * @return {object} response message
+   * @memberOf UsersController
+   */
+  static createUsers(req, res) {
+    model.User
+      .create(req.userInput)
+      .then((user) => {
+        const token = Auth.getToken(user);
+        user = Helper.userProfile(user);
+        return res.status(201)
+          .send({
+            message: 'Your account has been created successfully',
+            token,
+            user
+          });
+      })
+      .catch(error =>
+        res.status(400)
+          .send({
+            errorArray: Helper.errorArray(error)
+          }));
+  }
 }
 
 module.exports = UsersController;

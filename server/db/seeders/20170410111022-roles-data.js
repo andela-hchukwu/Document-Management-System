9d41b76d9c0b16fd0ca90581dc7e3bfda8fe778c
henry <henry.chukwu@andela.com>
2017-04-10 12:17:12 +0100

    chore(seeders-file): create seeders file [Start #142272055]

---
+++ server/db/seeders/20170410111022-roles-data.js
@@ -0,0 +1,25 @@
+'use strict';
+
+module.exports = {
+  up: function (queryInterface, Sequelize) {
+    return queryInterface.bulkInsert('Roles', [{
+      title: 'admin',
+      createdAt: '2017-03-31 13:51:40.653+01',
+      updatedAt: '2017-03-31 13:51:40.653+01'
+    }, {
+      title: 'regular',
+      createdAt: '2017-03-31 13:51:40.653+01',
+      updatedAt: '2017-03-31 13:51:40.653+01'
+    }], {});
+  },
+
+  down: function (queryInterface, Sequelize) {
+    /*
+      Add reverting commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.bulkDelete('Person', null, {});
+    */
+  }
+};
